6f50e930eaa516cec5d1d798fc778bf05a24e061
Hyoeun Lee <dlgydms412@gmail.com>
2020-11-12 02:39:52 +0900

    debugging for case 1

--- main2(Bug).py
+++ main2(Bug).py
@@ -26,12 +26,7 @@ ext_table = {
     'task_C': [0, 0, 0, 1],
     'task_D': [0, 0, 0, 1],
 }
-ext_table = {
-    'task_A': [0, 0.1, 0.1, 0, 0.2, 0.3, 0.2, 0.1],
-    'task_B': [0, 0.1, 0.1, 0.3, 0.1, 0.2, 0.1, 0.1],
-    'task_C': [0, 0.1, 0.3, 0.3, 0.2, 0.1],
-    'task_D': [0, 0.3, 0, 0.3, 0, 0.4],
-}
+
 
 def sys_simulation():
     simsys = SIMSYS(feature_set, ext_table, task_graph, max_time)
@@ -39,10 +34,11 @@ def sys_simulation():
     
     while simsys.current_time < simsys.max_time:
         print("...........................")
-        cpu_idx, next_evt = simsys.find_min_event_time()
-        simsys.update_system_status(cpu_idx, next_evt)
-        simsys.cpus[cpu_idx].print_status("")
-        input()
+        cpu_list, next_time, next_tasks = simsys.find_min_event_time()
+        simsys.update_system_status(cpu_list, next_time, next_tasks)
+        for cpu_idx in cpu_list:
+            simsys.cpus[cpu_idx].print_status("")
+        #input()
     return simsys.gathered_rtl, simsys.gathered_msg
 
 response_time_list, e2eL_msgs = sys_simulation()
